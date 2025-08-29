5d5041f23df70500c74954e6933fe4d240e61796
VinamarSingh <Vinamar.SINGH@student.tafesa.edu.au>
2025-08-30 09:05:28 +0930

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -30,7 +30,7 @@ namespace Calculator
 			if (e.PrelaunchActivated == false)
 			{
 				if (rootFrame.Content == null)
-					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+					rootFrame.Navigate(typeof(MainMenu), e.Arguments);
 
 				Window.Current.Activate();
 			}
@@ -46,4 +46,5 @@ namespace Calculator
 			e.SuspendingOperation.GetDeferral().Complete();
 		}
 	}
+
 }
